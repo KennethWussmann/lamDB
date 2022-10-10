b81d9ffcdd376f548da8148cc3a60899642d8fb4
Kenneth Wußmann <kenneth@wussmann.net>
2022-10-10 11:47:57 +0200

    Add litestream binary to engine layer

--- packages/engine-layer/src/build.ts
+++ packages/engine-layer/src/build.ts
@@ -1,12 +1,24 @@
-import { BinaryType, download } from '@prisma/fetch-engine';
-import { Platform } from '@prisma/get-platform';
+import fetchEngine from '@prisma/fetch-engine';
+import getPlatform from '@prisma/get-platform';
 import { access, mkdir, readFile, rename, rm, writeFile } from 'fs/promises';
 import { join } from 'path';
+import { $ } from 'zx';
+
+const { BinaryType, download } = fetchEngine;
+type Platform = getPlatform.Platform;
+type BinaryType = fetchEngine.BinaryType;
+
+type OS = 'darwin' | 'linux';
 
 const destination = join(process.cwd(), 'dist');
+const buildDirectory = join(process.cwd(), 'build');
 const engines: BinaryType[] = [BinaryType.queryEngine, BinaryType.migrationEngine];
-const target: Platform = (process.env.TARGET as Platform) ?? 'linux-arm64-openssl-3.0.x';
-const version = process.env.VERSION ?? 'ee0282f44ff27043cee9ae3e404033e6e7ec1748';
+const os: OS = (process.env.OS as OS) ?? 'linux';
+const prismaVersion = process.env.PRISMA_VERSION ?? 'ee0282f44ff27043cee9ae3e404033e6e7ec1748';
+const prismaTarget: Platform = os === 'darwin' ? 'darwin' : 'linux-arm64-openssl-3.0.x';
+const litestreamVersion = '0.3.9';
+const litestreamTarget = os === 'darwin' ? 'darwin-amd64' : 'linux-arm64-static';
+const litestreamArchiveType: 'zip' | 'tar.gz' = os === 'darwin' ? 'zip' : 'tar.gz';
 const enginesJsonPath = join(destination, 'engines.json');
 
 const exists = async (file: string): Promise<boolean> => {
@@ -24,48 +36,80 @@ const isCacheCompatible = async () => {
   }
   const cacheTarget = JSON.parse(await readFile(enginesJsonPath, 'utf8'));
   return (
-    cacheTarget.target === target &&
-    engines.every((engine) => cacheTarget.engines.includes(engine)) &&
-    version === cacheTarget.version
+    cacheTarget.prisma.target === prismaTarget &&
+    engines.every((engine) => cacheTarget.prisma.engines.includes(engine)) &&
+    cacheTarget.prisma.version === prismaVersion
   );
 };
 
+const downloadPrisma = async () => {
+  console.log('Downloading engines', { prismaVersion, engines, prismaTarget });
+  await download({
+    binaries: Object.fromEntries(engines.map((engine) => [engine, destination])),
+    version: prismaVersion,
+    showProgress: true,
+    printVersion: true,
+    binaryTargets: [prismaTarget],
+  });
+  console.log('Renaming binaries');
+  await Promise.all(
+    engines.map(async (engine) => {
+      await rename(join(destination, `${engine}-${prismaTarget}`), join(destination, `${engine}`));
+    }),
+  );
+};
+
+const downloadLitestream = async () => {
+  const downloadUrl = `https://github.com/benbjohnson/litestream/releases/download/v${litestreamVersion}/litestream-v${litestreamVersion}-${litestreamTarget}.${litestreamArchiveType}`;
+  const archivePath = join(buildDirectory, `litestream.${litestreamArchiveType}`);
+  await $`curl -L ${downloadUrl} -o ${archivePath}`;
+
+  if (litestreamArchiveType === 'tar.gz') {
+    await $`tar -xvf ${archivePath} -C ${destination}`;
+  } else {
+    await $`unzip ${archivePath} -d ${destination}`;
+  }
+};
+
 const build = async () => {
+  if (await exists(buildDirectory)) {
+    await rm(buildDirectory, {
+      recursive: true,
+    });
+  }
+  await mkdir(buildDirectory);
+
   if (await exists(destination)) {
     if (!(await isCacheCompatible())) {
-      console.log('Removing ./dist because downloaded binary is not compatible with selected target', target);
+      console.log('Removing ./dist because downloaded binary is not compatible');
       await rm(destination, {
         recursive: true,
       });
       await mkdir(destination);
     } else {
-      console.log('Using cached binaries', { version, engines, target });
+      console.log('Using cached binaries', { prismaVersion, engines, prismaTarget });
       return;
     }
   } else {
     await mkdir(destination);
   }
-  console.log('Downloading engines', { version, engines, target });
-  await download({
-    binaries: Object.fromEntries(engines.map((engine) => [engine, destination])),
-    version,
-    showProgress: true,
-    printVersion: true,
-    binaryTargets: [target],
-  });
-  console.log('Renaming binaries');
-  await Promise.all(
-    engines.map(async (engine) => {
-      await rename(join(destination, `${engine}-${target}`), join(destination, `${engine}`));
-    }),
-  );
+
+  await downloadPrisma();
+  await downloadLitestream();
+
   console.log('Writing engines.json file');
   await writeFile(
     enginesJsonPath,
     JSON.stringify({
-      engines,
-      target,
-      version: version ?? null,
+      prisma: {
+        engines,
+        target: prismaTarget,
+        version: prismaVersion ?? null,
+      },
+      litestream: {
+        target: litestreamTarget,
+        version: litestreamVersion,
+      },
     }),
   );
   console.log('Done');

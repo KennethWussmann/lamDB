362262cba33bdf61f684173ec79be7b212e58443
Kenneth Wußmann <kenneth@wussmann.net>
2022-10-21 18:48:30 +0200

    Adjust engine layer path

--- packages/infrastructure/lib/lamDBEngineLayer.ts
+++ packages/infrastructure/lib/lamDBEngineLayer.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 export class LamDBEngineLayer extends LayerVersion {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
-      code: Code.fromAsset(join(__dirname, '..', 'build', 'engine-layer')),
+      code: Code.fromAsset(join(__dirname, '..', 'dist', 'engine-layer')),
       description: 'Prisma Engine binaries',
       compatibleArchitectures: [Architecture.ARM_64],
     });

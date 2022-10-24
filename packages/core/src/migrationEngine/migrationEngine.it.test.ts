cd10a90c0cd686e4754d9be8b24e747cb1760378
Kenneth Wußmann <kenneth@wussmann.net>
2022-10-24 10:36:55 +0200

    Add retry test and add expect

--- packages/core/src/migrationEngine/migrationEngine.it.test.ts
+++ packages/core/src/migrationEngine/migrationEngine.it.test.ts
@@ -1,4 +1,5 @@
-import { databasePath, migrationEnginePath, prismaSchemaPath } from '../../test/binaryPaths';
+import { databaseMigrationLockPath, databasePath, migrationEnginePath, prismaSchemaPath } from '../../test/binaryPaths';
+import { exists } from '../utils';
 import { MigrationEngine } from './migrationEngine';
 
 const engine = new MigrationEngine({
@@ -8,9 +9,14 @@ const engine = new MigrationEngine({
 });
 
 describe('migrationEngine', () => {
-  it('applies migrations to fresh database', async () => {
+  it('applies migrations to fresh database and trying to reapply is skipped', async () => {
     const appliedMigrations = await engine.apply();
+    const lockFileExists = await exists(databaseMigrationLockPath);
 
+    expect(lockFileExists).toBeTruthy();
     expect(appliedMigrations).toMatchSnapshot();
+
+    const retryAppliedMigrations = await engine.apply();
+    expect(retryAppliedMigrations).toHaveLength(0);
   });
 });

289ad869393857688388f6ba79f5f0056e8a6f2e
Kenneth Wußmann <kenneth@wussmann.net>
2022-11-19 15:46:05 +0100

    Add reader writerHandler test

--- packages/lambda/src/handler/readerWriterHandler.ts
+++ packages/lambda/src/handler/readerWriterHandler.ts
@@ -2,25 +2,27 @@ import { APIGatewayProxyEventV2, Context, Handler } from 'aws-lambda';
 import { fromApiGatwayRequest, isRequest, toApiGatewayResponse } from '../utils';
 import { Request, tracer } from '@lamdb/commons';
 import { defaultApplicationContext } from '../applicationContext';
+import { LamDBService } from '@lamdb/core';
 
-class ReaderWriterHandler {
-  constructor(private endpointType: 'reader' | 'writer') {}
+export class ReaderWriterHandler {
+  constructor(
+    private endpointType: 'reader' | 'writer',
+    private service: LamDBService = defaultApplicationContext.service,
+  ) {}
 
   @tracer.captureLambdaHandler({ captureResponse: false })
   async handler(request: APIGatewayProxyEventV2 | Request, _: Context) {
     tracer.annotateColdStart();
     tracer.getSegment().addMetadata('initType', process.env.AWS_LAMBDA_INITIALIZATION_TYPE);
-    const { service } = defaultApplicationContext;
 
-    if (isRequest(request)) {
-      return toApiGatewayResponse(await service.execute(request, this.endpointType));
-    }
-    return toApiGatewayResponse(await service.execute(fromApiGatwayRequest(request), this.endpointType));
+    const lamDBRequest = isRequest(request) ? request : fromApiGatwayRequest(request);
+
+    return toApiGatewayResponse(await this.service.execute(lamDBRequest, this.endpointType));
   }
 }
 
 const readerClass = new ReaderWriterHandler('reader');
 const writerClass = new ReaderWriterHandler('writer');
 
-export const readerHandler: Handler = readerClass.handler;
-export const writerHandler: Handler = writerClass.handler;
+export const readerHandler: Handler = readerClass.handler.bind(readerClass);
+export const writerHandler: Handler = writerClass.handler.bind(writerClass);
